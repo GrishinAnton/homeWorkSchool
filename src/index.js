1202524154e9c378931de8e614a3b9b8341b5788
Grishin Anton <grishin_rostov@mail.ru>
2018-04-28 10:23:22 +0300

    Добавил задачу 5,6

--- src/index.js
+++ src/index.js
@@ -83,6 +83,13 @@ function returnCounter(number) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
+    var arr = [];
+
+    for (var i = 0; i < arguments.length; i++) {
+        arr.push(arguments[i]);
+    }
+
+    return arr;
 }
 
 /*
@@ -100,7 +107,14 @@ function returnArgumentsArray() {
 
    console.log(newSum()) выведет 6
  */
-function bindFunction(fn) {
+function bindFunction(fn, ...args) {
+    var f = fn;
+    var arg = args;
+
+    return function() {      
+
+        return f.apply(this, arg)
+    };
 }
 
 export {
